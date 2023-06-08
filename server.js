b6f285c36f810243728554ecd3f8b75eab1c06c6
Peki <112956568+akipeki@users.noreply.github.com>
2023-06-08 17:31:24 +0300

    changes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const PORT = 8000
-const express = require('express')
-const cors = require('cors')
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-
-const API_KEY = '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'
+const API_KEY = process.env.API_KEY
 
 app.post('/completions', async (req, res) => {
     const options = {
@@ -17,17 +17,17 @@ app.post('/completions', async (req, res) => {
         },
         body: JSON.stringify({
             model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: req.body.message}],
-            max_tokens: 100,
+            messages: [{ role: "user", content: req.body.message }],
+            max_tokens: 120,
         })
     }
     try {
-    const response = await fetch('https://api.openai.com/v1/chat/completions')
-const data = await response.json()
-res.send(data)
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json();
+        res.send(data);
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
-})
+});
 
-app.listen(PORT, () => console.log('Your server is running on Port' + PORT))
+app.listen(PORT, () => console.log('Your server is running on Port' + PORT));
--- src/App.js
+++ src/App.js
@@ -1,61 +1,103 @@
-import { useState, useEffect}  from 'react'
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-  const [ value, setValue] = useState(null)
-  const [ message, setMessage ] = useState(null)
-  const [ previousChats, setPreviousChats] = useState([])
- 
-const getMessages = async () => {
-  const options = {
-    method: "POST",
-    body : JSON.stringify({
-     // message: "hello how are you?"
-        message: value
-    }),
-    headers: {
-      "Content-Type": "application/json"
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState(null);
+
+  const createNewChat = () => {
+    setMessage(null)
+    setValue("")
+    setCurrentTitle(null)
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle)
+    setMessage(null)
+    setValue("")
+  }
+
+  const getMessages = async () => {
+    const prompt = "You are a kindergarten teacher with good pedagogical skills and tendency to answer with metaphors and some inspirational extra information regarding the question. Answer with max 110 tokens. This time the question is:";
+    const completeQuestion = `${prompt} ${value}`;
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: completeQuestion
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    try {
+      const response = await fetch("http://localhost:8000/completions", options);
+      const data = await response.json();
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value)
     }
 
-  }
-  try {
-    const response = await fetch('http://localhost:8000/completions', options)
-    const data = await response.json()
-    setMessage(data.choices[0].message)
-  } catch (error) {
-    console.error(error)
-  }
-}
+    if (currentTitle && value && message) {
+      setPreviousChats(prevChats => (
+        [...prevChats,
+        {
+          title: currentTitle,
+          role: "user",
+          content: value
+        }, {
+          title: currentTitle,
+          role: message.role,
+          content: message.content
+        }]
+      ))
+    }
+  }, [message, currentTitle])
 
-console.log(message)
+  console.log(previousChats)
+
+  const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle)
+  const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)))
 
   return (
     <div className="app">
-   <section className="side-bar">
-    <button>+ New chat</button>
-    <ul className="history">
-      <li>BLUGH</li>
-      </ul>
-    <nav>
-      <p className="credits">Made by Peki</p>
-    </nav>
-   </section>
-   <section className="main">
-    <h1>PekiGPT</h1>
-    <ul className="feed">
-
-    </ul>
-    <div className="bottom-section">
-      <div className="input-container">
-        <input value={value} onChange={(e) => setValue(e.target.value)}/>
-        <div id="submit" onClick={getMessages}>➢</div>
-      </div>
-      <p className="info">
-      Chat GPT Mar 14 Version. Free Research Previev. 
-      Our Goal is to make AI systems more natural and safe to interact with.
-      Your feedbackj will help us improve.
-      </p>
-    </div>
-   </section>
+      <section className="side-bar">
+        <button onClick={createNewChat}>+ New chat</button>
+        <ul className="history">
+          <li>{uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}</li>
+        </ul>
+        <nav>
+          <p className="credits">PEK-1</p>
+        </nav>
+      </section>
+      <section className="main">
+        {!currentTitle && <h1>Junior Science Lab®</h1>}
+        <ul className="feed">
+          {currentChat.map((chatMessage, index) => <li key={index}>
+            <p className='role'>{chatMessage.role}</p>
+            <p>{chatMessage.content}</p>
+          </li>)}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div id="submit" onClick={getMessages}>➢</div>
+          </div>
+          <p className="info">
+            Dear Juior Science Lab® student. Do you have any questions for me?
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
