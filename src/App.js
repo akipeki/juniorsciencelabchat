dac53be2488ffd760c825ace0a5ac518578c46e2
Peki <112956568+akipeki@users.noreply.github.com>
2023-06-08 11:16:52 +0300

    changes towards the actual site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect}  from 'react'
 
-function App() {
+const App = () => {
+  const [ value, setValue] = useState(null)
+  const [ message, setMessage ] = useState(null)
+  const [ previousChats, setPreviousChats] = useState([])
+ 
+const getMessages = async () => {
+  const options = {
+    method: "POST",
+    body : JSON.stringify({
+     // message: "hello how are you?"
+        message: value
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+
+  }
+  try {
+    const response = await fetch('http://localhost:8000/completions', options)
+    const data = await response.json()
+    setMessage(data.choices[0].message)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+console.log(message)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+   <section className="side-bar">
+    <button>+ New chat</button>
+    <ul className="history">
+      <li>BLUGH</li>
+      </ul>
+    <nav>
+      <p className="credits">Made by Peki</p>
+    </nav>
+   </section>
+   <section className="main">
+    <h1>PekiGPT</h1>
+    <ul className="feed">
+
+    </ul>
+    <div className="bottom-section">
+      <div className="input-container">
+        <input value={value} onChange={(e) => setValue(e.target.value)}/>
+        <div id="submit" onClick={getMessages}>➢</div>
+      </div>
+      <p className="info">
+      Chat GPT Mar 14 Version. Free Research Previev. 
+      Our Goal is to make AI systems more natural and safe to interact with.
+      Your feedbackj will help us improve.
+      </p>
+    </div>
+   </section>
     </div>
   );
 }
